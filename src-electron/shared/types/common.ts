c060d0c336a195bba865375340115c199b5def42
ParticleG <particle_g@outlook.com>
2024-01-17 10:50:54 +0800

    - Remove buggy same-name class `CaretPosition` - Remove unused interface `Completions` - Add interface `SymbolInfo`

--- src-electron/shared/types/common.ts
+++ src-electron/shared/types/common.ts
@@ -3,24 +3,15 @@ export interface CaretPosition {
   line: number;
 }
 
-export class CaretPosition {
-  constructor(line = -1, character = -1) {
-    this.character = character;
-    this.line = line;
-  }
-
-  isValid() {
-    return this.line >= 0 && this.character >= 0;
-  }
-}
-
 export enum CompletionType {
   Function = 'Function',
   Line = 'Line',
   Snippet = 'Snippet',
 }
 
-export interface Completions {
-  contents: string[];
-  type: CompletionType;
+export interface SymbolInfo {
+  endLine: number;
+  name: string;
+  path: string;
+  startLine: number;
 }
