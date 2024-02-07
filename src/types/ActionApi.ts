0a85058a817832576c51aaf15303286e17bcf6b7
ParticleG <particle_g@outlook.com>
2024-02-07 12:15:58 +0800

    - Add class `ActionApi`

---
+++ src/types/ActionApi.ts
@@ -0,0 +1,32 @@
+import { ActionMessageMapping } from 'shared/types/ActionMessage';
+
+export class ActionApi {
+  private readonly _baseName: string;
+  private _registeredActionTypes: Set<keyof ActionMessageMapping> = new Set();
+
+  constructor(baseName: string) {
+    this._baseName = baseName;
+  }
+
+  register<T extends keyof ActionMessageMapping>(
+    actionType: T,
+    callback: (data: ActionMessageMapping[T]['data']) => void
+  ): void {
+    window.actionApi.register(
+      actionType,
+      this._baseName + actionType,
+      callback
+    );
+    this._registeredActionTypes.add(actionType);
+  }
+
+  unregister(): void {
+    for (const registeredActionType of this._registeredActionTypes) {
+      window.actionApi.unregister(
+        registeredActionType,
+        this._baseName + registeredActionType
+      );
+    }
+    this._registeredActionTypes.clear();
+  }
+}
