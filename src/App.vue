e0a3baf9eae9f72f5ac9661521dbccd7db9e8513
ParticleG <particle_g@outlook.com>
2024-02-07 12:05:09 +0800

    - Add the ability to reload the whole component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+import { ActionType } from 'shared/types/ActionMessage';
 import { useHighlighter } from 'stores/highlighter';
 import { useSettingsStore } from 'stores/settings';
+import { ActionApi } from 'types/ActionApi';
+
+const baseName = 'web.app.';
 
 const { initialize } = useHighlighter();
 const { applyDarkMode } = useSettingsStore();
 
 initialize().then();
 applyDarkMode();
+
+const reloadKey = ref(false);
+
+const actionApi = new ActionApi(baseName);
+onMounted(() => {
+  actionApi.register(ActionType.RouterReload, () => {
+    reloadKey.value = !reloadKey.value;
+  });
+});
+onBeforeUnmount(() => {
+  actionApi.unregister();
+});
 </script>
 
 <template>
-  <router-view />
+  <router-view :key="reloadKey" />
 </template>
