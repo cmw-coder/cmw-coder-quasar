e3e746374193b9146cc9194411bad5983a22a804
ParticleG <particle_g@outlook.com>
2024-02-27 17:06:51 +0800

    - Init OverviewPage.vue

---
+++ src/pages/OverviewPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <q-page class="row justify-evenly q-pa-xl">
+    <div class="col-grow column">
+      Comware Coder V1.0.3
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+
+</style>
