5be1bbd6ad38c10f977b1e7027d15bed5ac37dec
zengziyu <zeng.ziyu@h3c.com>
2024-05-27 18:20:52 +0800

    - Fix window activation issue in AppService

--- src-electron/main/services/AppService/index.ts
+++ src-electron/main/services/AppService/index.ts
@@ -126,7 +126,7 @@ export class AppService implements AppServiceTrait {
 
       this._windowService.trayIcon.activate();
       // 激活代码窗口
-      // this._windowService.getWindow(WindowType.Completions).activate();
+      this._windowService.getWindow(WindowType.Completions).activate();
 
       // 引导配置基础环境（黄、绿区 | 红区 | 路由红区）
       if (config.networkZone === NetworkZone.Unknown) {
