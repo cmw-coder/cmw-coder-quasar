889202c9d1e39d964d5cb7928ef5074c9aca07e4
ParticleG <particle_g@outlook.com>
2024-05-29 07:27:03 +0800

    - Update `DeveloperPage`

--- src/pages/DeveloperPage.vue
+++ src/pages/DeveloperPage.vue
@@ -20,7 +20,7 @@ const i18n = (relativePath: string) => {
 const websocketService = useService(ServiceType.WEBSOCKET);
 const windowService = useService(ServiceType.WINDOW);
 
-const currentTab = ref('');
+const currentIndex = ref(0);
 const caret = reactive({
   character: 0,
   line: 0,
@@ -85,7 +85,7 @@ const calculate = async () => {
       prefixLines.join('\n'),
       suffixLines.join('\n'),
     );
-    currentTab.value = similarSnippets.value[0].path;
+    currentIndex.value = 0;
   } else {
     similarSnippets.value = [];
   }
@@ -102,142 +102,147 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <q-page class="row justify-start q-col-gutter-x-md q-pa-md">
-    <div class="column col-6 q-gutter-y-md">
-      <q-card bordered flat>
-        <q-card-section class="q-gutter-y-md">
-          <div class="row items-baseline q-col-gutter-x-md">
-            <div class="text-h5">
-              {{ i18n('labels.currentFile') }}
-            </div>
-            <q-input
-              class="col-grow"
-              clearable
-              dense
-              :error="currentFile.error"
-              label="Input current file path"
-              :loading="currentFile.loading"
-              outlined
-              v-model="currentFile.path"
-              @blur="getCurrentFileContent"
-              @keyup.enter.stop="getCurrentFileContent"
-            />
-          </div>
-        </q-card-section>
-        <q-card-section class="q-gutter-y-md">
-          <div class="row items-center q-col-gutter-x-md">
-            <div class="text-h5">Caret Position</div>
-            <q-input
-              class="col-grow"
-              dense
-              label="Cursor line"
-              outlined
-              type="number"
-              v-model="caret.line"
-            />
-            <q-input
-              class="col-grow"
-              dense
-              label="Cursor character"
-              outlined
-              type="number"
-              v-model="caret.character"
-            />
-          </div>
-          <q-card bordered flat>
-            <q-scroll-area class="full-width" style="height: 600px">
-              <code-block :html="currentFile.content" style="max-width: 80ch" />
-            </q-scroll-area>
-          </q-card>
-        </q-card-section>
-      </q-card>
-    </div>
-    <div class="column col-6 q-gutter-y-md">
-      <q-card bordered flat>
-        <q-card-section class="q-gutter-y-md">
-          <div class="text-h5">
-            {{ i18n('labels.referenceFiles') }}
-          </div>
-          <div class="row q-col-gutter-x-md">
-            <q-input
-              class="col-grow"
-              clearable
-              :error="referenceFolder.error"
-              label="Input current file path"
-              :loading="referenceFolder.loading"
-              outlined
-              v-model="referenceFolder.path"
-              @blur="checkReferenceFolder"
-              @keyup.enter.stop="checkReferenceFolder"
-            >
-              <template v-slot:after>
-                <q-btn
-                  class="full-height"
-                  color="primary"
-                  :disable="
-                    !currentFile.path ||
-                    currentFile.error ||
-                    currentFile.loading ||
-                    !referenceFolder.path ||
-                    referenceFolder.error ||
-                    referenceFolder.loading
-                  "
-                  icon="mdi-file-find"
-                  outline
-                  @click="calculate"
+  <q-page class="row q-pa-md">
+    <q-card class="col-grow" bordered flat>
+      <q-splitter :model-value="50">
+        <template v-slot:before>
+          <div class="column q-pa-md">
+            <div class="column q-gutter-y-md">
+              <div class="row items-baseline q-col-gutter-x-md">
+                <div class="text-h5">
+                  {{ i18n('labels.currentFile') }}
+                </div>
+                <q-input
+                  class="col-grow"
+                  clearable
+                  dense
+                  :error="currentFile.error"
+                  label="Input current file path"
+                  :loading="currentFile.loading"
+                  outlined
+                  v-model="currentFile.path"
+                  @blur="getCurrentFileContent"
+                  @keyup.enter.stop="getCurrentFileContent"
                 />
-              </template>
-            </q-input>
-          </div>
-        </q-card-section>
-        <q-card-section v-if="similarSnippets.length" class="q-gutter-y-md">
-          <div class="text-h5">Similar Snippets</div>
-          <q-card bordered flat>
-            <q-tabs
-              class="text-grey"
-              active-color="primary"
-              align="justify"
-              dense
-              indicator-color="primary"
-              narrow-indicator
-              no-caps
-              v-model="currentTab"
-              style="max-width: 733px"
-            >
-              <q-tab
-                v-for="(similarSnippet, index) in similarSnippets"
-                :key="index"
-                :label="similarSnippet.path.split('/').at(-1)"
-                :name="similarSnippet.path"
-              >
-                <q-tooltip>
-                  {{ similarSnippet.path }}
-                </q-tooltip>
-              </q-tab>
-            </q-tabs>
-          </q-card>
-          <q-card bordered flat>
-            <q-tab-panels v-model="currentTab">
-              <q-tab-panel
-                v-for="(similarSnippet, index) in similarSnippets"
-                :key="index"
-                class="q-gutter-y-xs"
-                :name="similarSnippet.path"
-              >
-                <div>Score: {{ similarSnippet.score }}</div>
-                <q-separator />
-                <q-scroll-area class="full-width" style="height: 420px">
+              </div>
+            </div>
+            <div class="column q-gutter-y-md">
+              <div class="row items-center q-col-gutter-x-md">
+                <div class="text-h5">Caret Position</div>
+                <q-input
+                  class="col-grow"
+                  dense
+                  label="Cursor line"
+                  outlined
+                  type="number"
+                  v-model="caret.line"
+                />
+                <q-input
+                  class="col-grow"
+                  dense
+                  label="Cursor character"
+                  outlined
+                  type="number"
+                  v-model="caret.character"
+                />
+              </div>
+              <q-card bordered flat>
+                <q-scroll-area class="full-width" style="height: 633px">
                   <code-block
-                    :html="codeToHtml(similarSnippet.content, 'c')"
+                    :html="currentFile.content"
                     style="max-width: 80ch"
                   />
                 </q-scroll-area>
-              </q-tab-panel>
-            </q-tab-panels>
-          </q-card>
-        </q-card-section>
-      </q-card>
-    </div>
+              </q-card>
+            </div>
+          </div>
+        </template>
+        <template v-slot:after>
+          <div class="column q-pa-md">
+            <div class="column q-gutter-y-md">
+              <div class="text-h5">
+                {{ i18n('labels.referenceFiles') }}
+              </div>
+              <div class="row q-col-gutter-x-md">
+                <q-input
+                  class="col-grow"
+                  clearable
+                  dense
+                  :error="referenceFolder.error"
+                  :loading="referenceFolder.loading"
+                  outlined
+                  v-model="referenceFolder.path"
+                  @blur="checkReferenceFolder"
+                  @keyup.enter.stop="checkReferenceFolder"
+                >
+                  <template v-slot:after>
+                    <q-btn
+                      class="full-height"
+                      color="primary"
+                      :disable="
+                        !currentFile.path ||
+                        currentFile.error ||
+                        currentFile.loading ||
+                        !referenceFolder.path ||
+                        referenceFolder.error ||
+                        referenceFolder.loading
+                      "
+                      icon="mdi-file-find"
+                      outline
+                      @click="calculate"
+                    />
+                  </template>
+                </q-input>
+              </div>
+            </div>
+            <div class="column q-gutter-y-sm">
+              <div class="text-h5">Similar Snippets</div>
+              <q-card bordered flat>
+                <q-scroll-area class="full-width" style="height: 150px">
+                  <q-list dense separator>
+                    <q-item
+                      v-for="(similarSnippet, index) in similarSnippets"
+                      :key="index"
+                      :active="index === currentIndex"
+                      active-class="bg-blue-2"
+                      clickable
+                      dense
+                      @click="currentIndex = index"
+                    >
+                      <q-item-section>
+                        <q-item-label>
+                          {{ similarSnippet.path.split('/').at(-1) }}
+                        </q-item-label>
+                        <q-item-label caption>
+                          {{ similarSnippet.path }}
+                        </q-item-label>
+                      </q-item-section>
+                      <q-item-section side>
+                        Score: {{ similarSnippet.score.toFixed(4) }}
+                      </q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-scroll-area>
+              </q-card>
+              <q-card v-if="similarSnippets.length" bordered flat>
+                <q-scroll-area
+                  v-if="similarSnippets[currentIndex]"
+                  class="full-width"
+                  style="height: 441px"
+                >
+                  <code-block
+                    :html="
+                      codeToHtml(similarSnippets[currentIndex].content, 'c')
+                    "
+                    style="max-width: 80ch"
+                  />
+                </q-scroll-area>
+              </q-card>
+            </div>
+          </div>
+        </template>
+      </q-splitter>
+    </q-card>
   </q-page>
 </template>
 
