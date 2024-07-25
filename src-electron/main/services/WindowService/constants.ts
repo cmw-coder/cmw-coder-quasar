8d4cb02bb84f3aaf8bf0fed1eaa8abe5de6f6f01
ParticleG <particle_g@outlook.com>
2024-07-26 05:03:26 +0800

    - Fix `treeSitterFolder`

--- src-electron/main/services/WindowService/constants.ts
+++ src-electron/main/services/WindowService/constants.ts
@@ -1,5 +1,5 @@
 import path from 'path';
 
 export const treeSitterFolder = process.env.PROD
-  ? `"${path.join(process.resourcesPath, 'tree-sitter')}"`
+  ? path.join(process.resourcesPath, 'tree-sitter')
   : path.join(__dirname, '../../src-electron/assets/tree-sitter');
