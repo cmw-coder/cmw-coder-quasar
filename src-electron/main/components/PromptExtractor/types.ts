4a21a6b50e5de82f784370197d56019e753e8f97
zengziyu <zeng.ziyu@h3c.com>
2024-08-02 10:11:23 +0800

    - Add log.debug to getBoundingPrefix and removeFunctionHeader

--- src-electron/main/components/PromptExtractor/types.ts
+++ src-electron/main/components/PromptExtractor/types.ts
@@ -45,7 +45,9 @@ export class PromptElements {
   constructor(prefix: string, suffix: string) {
     this.prefix = prefix.trimStart();
     this.suffix = suffix.trimEnd();
+    log.debug('getBoundingPrefix before', this.prefix);
     this.currentFilePrefix = getBoundingPrefix(this.prefix) ?? this.prefix;
+    log.debug('getBoundingPrefix after', this.currentFilePrefix);
   }
 
   async stringify(completionType: CompletionType) {
@@ -59,10 +61,12 @@ export class PromptElements {
       '%{NeighborSnippet}%',
       this.neighborSnippet || '',
     );
+    log.debug('removeFunctionHeader before', this.currentFilePrefix);
     question = question.replaceAll(
       '%{CurrentFilePrefix}%',
       removeFunctionHeader(this.currentFilePrefix, completionType),
     );
+    log.debug('removeFunctionHeader after', this.currentFilePrefix);
     question = question.replaceAll(
       '%{NearCode}%',
       removeFunctionHeader(
