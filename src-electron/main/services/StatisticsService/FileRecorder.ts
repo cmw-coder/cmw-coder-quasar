840b29c1cc9a25e172163c5550a7ba1024c6b5ee
zengziyu <zeng.ziyu@h3c.com>
2024-11-04 13:56:42 +0800

    - Update FileRecorder to track file modification time and content changes

--- src-electron/main/services/StatisticsService/FileRecorder.ts
+++ src-electron/main/services/StatisticsService/FileRecorder.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, stat } from 'fs/promises';
 import iconv from 'iconv-lite';
 import { container } from 'main/services';
 import { ServiceType } from 'shared/types/service';
@@ -10,16 +10,24 @@ const DESTROY_TIME = 1000 * 60 * 30;
 export class FileRecorder {
   filePath: string;
   projectId: string;
-  fileContent = '';
-  lastChangedTime = Date.now();
+  lastFileContent = '';
+  lastModifiedTime = new Date();
   onDestroy: () => void = () => {};
 
   constructor(filePath: string, projectId: string) {
     this.filePath = filePath;
     this.projectId = projectId;
     this.readFileContent().then((content) => {
-      this.fileContent = content;
+      this.lastFileContent = content;
     });
+    this.getFileModifiedTime().then((mtime) => {
+      this.lastModifiedTime = mtime;
+    });
+  }
+
+  async getFileModifiedTime() {
+    const { mtime } = await stat(this.filePath);
+    return mtime;
   }
 
   async readFileContent() {
@@ -30,24 +38,25 @@ export class FileRecorder {
   async calculate() {
     // TODO
     // 通过读取文件更改时间判断文件是否发生变化
-    const newFileContent = await this.readFileContent();
-    if (newFileContent === this.fileContent) {
-      // 文件内容没有变化
-      if (Date.now() - this.lastChangedTime >= DESTROY_TIME) {
+    const newModifiedTime = await this.getFileModifiedTime();
+    if (newModifiedTime.getTime() === this.lastModifiedTime.getTime()) {
+      // 文件没有变化
+      if (Date.now() - this.lastModifiedTime.getTime() >= DESTROY_TIME) {
         this.onDestroy();
       }
       return undefined;
     } else {
-      // 文件内容发生变化
+      // 文件发生变化
+      const newFileContent = await this.readFileContent();
       const diffSubprocess = container
         .get<WindowService>(ServiceType.WINDOW)
         .getWindow(WindowType.Completions).diffSubprocess;
       const diffResult = await diffSubprocess.proxyFn.diffLine(
-        this.fileContent,
+        this.lastFileContent,
         newFileContent,
       );
-      this.fileContent = newFileContent;
-      this.lastChangedTime = Date.now();
+      this.lastFileContent = newFileContent;
+      this.lastModifiedTime = newModifiedTime;
       return diffResult;
     }
   }
