a58353fbcf2345cef77fdd779c52e7c06fedf36b
ParticleG <particle_g@outlook.com>
2024-11-06 09:43:11 +0800

    - Remove unused methods - Update dependencies

--- src-electron/main/components/PromptExtractor/utils.ts
+++ src-electron/main/components/PromptExtractor/utils.ts
@@ -1,45 +1,6 @@
-import { existsSync, promises } from 'fs';
-import { decode } from 'iconv-lite';
-
-import { REGEXP_WORD } from 'main/components/PromptExtractor/constants';
-import { TextDocument } from 'main/types/TextDocument';
-import { Position } from 'main/types/vscode/position';
 import { deleteComments } from 'main/utils/common';
 import { NEW_LINE_REGEX } from 'shared/constants/common';
 import { CompletionType } from 'shared/types/common';
-
-const { readFile } = promises;
-
-/**
- * @deprecated
- */
-// 找到第一个函数注释头的结尾
-export const getBoundingPrefix = (input: string): string | undefined => {
-  const lines = input.split(NEW_LINE_REGEX);
-  const firstCommentEndLine = lines.findIndex((line) =>
-    /^\/\/.*|^\S+.*?\*\/\s*$/.test(line),
-  );
-  if (firstCommentEndLine !== -1) {
-    return lines.slice(firstCommentEndLine + 1).join('\n');
-  }
-};
-
-/**
- * @deprecated
- */
-export const getBoundingSuffix = (input: string): string | undefined => {
-  const lines = input.split(NEW_LINE_REGEX);
-  const lastFunctionEndLine = lines.findLastIndex((line) => /^}\S*/.test(line));
-  if (lastFunctionEndLine !== -1) {
-    return lines.slice(0, lastFunctionEndLine + 1).join('\n');
-  }
-  const lastCommentStartLine = lines.findLastIndex((line) =>
-    /^\/\*.*/.test(line),
-  );
-  if (lastCommentStartLine !== -1) {
-    return lines.slice(0, lastCommentStartLine).join('\n');
-  }
-};
 
 export const getFunctionPrefix = (input: string): string | undefined => {
   const lines = input.split(NEW_LINE_REGEX);
@@ -63,43 +24,6 @@ export const getFunctionSuffix = (input: string): string | undefined => {
   }
 };
 
-export const getAllOtherTabContents = async (
-  tabPaths: string[],
-): Promise<{ path: string; content: string }[]> => {
-  return (
-    await Promise.all(
-      tabPaths
-        .filter((tabPath) => existsSync(tabPath))
-        .map((tabPath) => readFile(tabPath)),
-    )
-  ).map((tabContent, index) => ({
-    path: tabPaths[index],
-    content: decode(tabContent, 'gb2312'),
-  }));
-};
-
-export const getRemainedCodeContents = (
-  document: TextDocument,
-  position: Position,
-  prefix: string,
-  suffix: string,
-): {
-  before: string[];
-  after: string[];
-} => {
-  const rawText = document.getText();
-  return {
-    before: separateTextByLine(
-      rawText.substring(0, document.offsetAt(position) - prefix.length),
-      true,
-    ),
-    after: separateTextByLine(
-      rawText.substring(document.offsetAt(position) + suffix.length),
-      true,
-    ),
-  };
-};
-
 export const removeFunctionHeader = (
   input: string,
   completionType: CompletionType,
@@ -118,63 +42,4 @@ export const separateTextByLine = (
   return rawText
     .split(NEW_LINE_REGEX)
     .filter((tabContentLine) => tabContentLine.trim().length > 0);
-};
-
-export const tokenize = (
-  rawText: string,
-  ignoreRules: Array<Set<string>>,
-  splitPattern: RegExp = REGEXP_WORD,
-): Set<string> => {
-  let tokens = rawText.split(splitPattern).filter((token) => token.length > 0);
-  ignoreRules.forEach(
-    (ignoreRule) => (tokens = tokens.filter((token) => !ignoreRule.has(token))),
-  );
-  return new Set(tokens);
-};
-
-export const getMostSimilarSnippetStartLine = (
-  candidateTokens: Array<Set<string>>,
-  referenceTokens: Set<string>,
-  windowSize: number,
-): {
-  startLine: number;
-  score: number;
-} => {
-  const currentMostSimilar = {
-    startLine: 0,
-    score: 0,
-  };
-
-  for (
-    let startLineIndex = 0;
-    startLineIndex + windowSize < candidateTokens.length;
-    startLineIndex++
-  ) {
-    const windowedCandidateTokens = new Set(
-      candidateTokens
-        .slice(startLineIndex, startLineIndex + windowSize)
-        .reduce(
-          (accumulatedTokens, targetLineTokens) =>
-            accumulatedTokens.concat([...targetLineTokens]),
-          Array<string>(),
-        ),
-    );
-
-    const intersectionTokens = new Set(
-      [...windowedCandidateTokens].filter((targetToken) =>
-        referenceTokens.has(targetToken),
-      ),
-    );
-
-    const currentScore =
-      intersectionTokens.size /
-      (windowedCandidateTokens.size +
-        referenceTokens.size -
-        intersectionTokens.size);
-    if (currentScore > currentMostSimilar.score) {
-      currentMostSimilar.startLine = startLineIndex;
-      currentMostSimilar.score = currentScore;
-    }
-  }
-  return currentMostSimilar;
 };
