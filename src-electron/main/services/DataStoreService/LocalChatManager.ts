afeba5423014df9cc46dbb9e7aff6b170efff018
ParticleG <particle_g@outlook.com>
2024-12-11 04:05:02 +0800

    - Import method as needed to save storage

--- src-electron/main/services/DataStoreService/LocalChatManager.ts
+++ src-electron/main/services/DataStoreService/LocalChatManager.ts
@@ -1,8 +1,16 @@
 import { app, shell } from 'electron';
-import path from 'path';
-import * as fs from 'fs';
-import { ChatFileContent, ChatItem } from 'shared/types/ChatMessage';
 import log from 'electron-log/main';
+import {
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  readFileSync,
+  unlinkSync,
+  writeFileSync,
+} from 'fs';
+import { join } from 'path';
+
+import { ChatFileContent, ChatItem } from 'shared/types/ChatMessage';
 
 const defaultChatFileContent: ChatFileContent = {
   createTime: new Date().valueOf(),
@@ -13,31 +21,31 @@ export class LocalChatManager {
   private readonly localChatDir: string;
 
   constructor() {
-    this.localChatDir = path.join(app.getPath('userData'), 'chatList');
+    this.localChatDir = join(app.getPath('userData'), 'chatList');
     this.checkChatListDir();
   }
 
   checkChatListDir() {
-    if (!fs.existsSync(this.localChatDir)) {
-      fs.mkdirSync(this.localChatDir);
+    if (!existsSync(this.localChatDir)) {
+      mkdirSync(this.localChatDir);
     }
-    const defaultChatFile = path.join(this.localChatDir, 'default_chat.json');
-    if (!fs.existsSync(defaultChatFile)) {
+    const defaultChatFile = join(this.localChatDir, 'default_chat.json');
+    if (!existsSync(defaultChatFile)) {
       defaultChatFileContent.createTime = new Date().valueOf();
-      fs.writeFileSync(defaultChatFile, JSON.stringify(defaultChatFileContent));
+      writeFileSync(defaultChatFile, JSON.stringify(defaultChatFileContent));
     }
   }
 
   getChatList() {
     const res: ChatItem[] = [];
-    const allFiles = fs.readdirSync(this.localChatDir);
+    const allFiles = readdirSync(this.localChatDir);
     for (let i = 0; i < allFiles.length; i++) {
       const file = allFiles[i];
       if (file.endsWith('_chat.json')) {
         const name = file.replaceAll('_chat.json', '');
         res.push({
           name,
-          filepath: path.join(this.localChatDir, file),
+          filepath: join(this.localChatDir, file),
         });
       }
     }
@@ -46,37 +54,37 @@ export class LocalChatManager {
 
   getChat(name: string) {
     const filename = `${name}_chat.json`;
-    const filepath = path.join(this.localChatDir, filename);
-    if (!fs.existsSync(filepath)) {
+    const filepath = join(this.localChatDir, filename);
+    if (!existsSync(filepath)) {
       defaultChatFileContent.createTime = new Date().valueOf();
-      fs.writeFileSync(filepath, JSON.stringify(defaultChatFileContent));
+      writeFileSync(filepath, JSON.stringify(defaultChatFileContent));
     }
-    const content = fs.readFileSync(filepath, 'utf-8');
+    const content = readFileSync(filepath, 'utf-8');
     return JSON.parse(content) as ChatFileContent;
   }
 
   newChat(name: string) {
     const filename = `${name}_chat.json`;
-    const filepath = path.join(this.localChatDir, filename);
-    if (!fs.existsSync(filepath)) {
+    const filepath = join(this.localChatDir, filename);
+    if (!existsSync(filepath)) {
       defaultChatFileContent.createTime = new Date().valueOf();
-      fs.writeFileSync(filepath, JSON.stringify(defaultChatFileContent));
+      writeFileSync(filepath, JSON.stringify(defaultChatFileContent));
     }
     return filepath;
   }
 
   saveChat(name: string, content: ChatFileContent) {
     const filename = `${name}_chat.json`;
-    const filepath = path.join(this.localChatDir, filename);
-    fs.writeFileSync(filepath, JSON.stringify(content));
+    const filepath = join(this.localChatDir, filename);
+    writeFileSync(filepath, JSON.stringify(content));
     return filepath;
   }
 
   deleteChat(name: string) {
     const filename = `${name}_chat.json`;
-    const filepath = path.join(this.localChatDir, filename);
-    if (fs.existsSync(filepath)) {
-      fs.unlinkSync(filepath);
+    const filepath = join(this.localChatDir, filename);
+    if (existsSync(filepath)) {
+      unlinkSync(filepath);
     }
   }
 
